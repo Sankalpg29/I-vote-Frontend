9c163796b6be416383b37bef6766f9c777d2b676
Sankalpg29 <gsankalp2920@gmail.com>
2025-01-17 09:35:07 +0530

    first commit

---
+++ src/pages/protectedroute.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Navigate} from 'react-router-dom';
+
+const ProtectedRoute = ({ element: Component, ...rest }) => {
+  
+  const token = localStorage.getItem('token');
+
+  return token ? <Component {...rest} /> : <Navigate to="/login" />;
+};
+
+
+export {ProtectedRoute};
+
